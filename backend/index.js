5f3c2b9d977605cf1143c989ba655154830adc08
KanishkRAJ29 <114609861+KanishkRAJ29@users.noreply.github.com>
2024-11-21 19:17:04 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  // res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
